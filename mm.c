8bdc4697f14e9fedef094bd07263a2b835c6b13a
ratna <manoj.br@gmail.com>
2018-05-21 23:34:23 -0700

    mm.

--- mm.c
+++ mm.c
@@ -1,27 +1,24 @@
-#include<stdio.h>
 #include "global.h"
 #include "list.h"
 #include "mm_ext.h"
-//#include "global.h"
 
-#define MEM_BLK_SHFT 16
+#define MEM_BLK_SHFT 12
 #define MEM_BLK_SIZE (1 << MEM_BLK_SHFT)
 
 typedef enum {INUSE, UNUSABLE, TOUSE} mb_state_t;
 
 struct ele_hdr {
 	uint32_t free:1;
-	uint32_t size:31;
+	uint32_t size:31; /* ele size, sizeof (ele_hdr) excluded */
 };
-
 
 struct mem_blk {
 	mb_state_t	state;
 	void		*head;
 	void		*tail;
 	void		*last;
+	void		*frst;
 	struct list_head list;
-	char		frst[0];
 };
 
 static struct list_head inuse_list;
@@ -32,12 +29,26 @@ static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static bool __isfull(mm_blk_t *mb)
 {
-	if (mb->head < mb->tail) {
-		return  (mb->head - (void *) mb->frst < mb_rsvd_size && 
-			(mb->last - mb->tail < mb_rsvd_size));
+	struct ele_hdr *hdr;
+
+	if (mb->head <= mb->tail) {
+		if (mb->head - mb->frst >= mb_rsvd_size)
+			return false;
+		if (mb->last - mb->tail + sizeof (struct ele_hdr) <
+		    mb_rsvd_size)
+			return true;
+		printf("%p %d %d -> ", mb, mb->head - mb->frst, 
+		    mb->tail - mb->frst); 
+		hdr = mb->head = mb->last - sizeof (struct ele_hdr);
+		printf("%d %d\n", mb, mb->head - mb->frst, mb->tail - mb->frst); 
+		hdr->free = 0;
+		hdr->size = 0;
 	} else { 
-		return  (mb->head - mb->tail < mb_rsvd_size);
+		if ((mb->head - mb->tail + sizeof(struct ele_hdr)) <
+		    mb_rsvd_size)
+			return true;
 	}
+	return false;
 }
 
 void mm_unreserve(mm_blk_t *mb)
@@ -45,16 +56,24 @@ void mm_unreserve(mm_blk_t *mb)
 	struct ele_hdr *hdr = mb->head;
 
 	assert(mb->state == INUSE);
-
+	assert(hdr->size < mb_rsvd_size - sizeof(struct ele_hdr));
 	mb->head -= hdr->size;
-	assert(mb->head >= (void *) (mb + 1));
+	hdr = mb->head = mb->head - sizeof (struct ele_hdr);
+	printf("%p unreserve %d %d\n", mb, mb->head - mb->frst,
+	    mb->tail - mb->frst); 
+	hdr->free = 0;
+	hdr->size = 0;
+	assert(mb->head >= mb->frst);
 
 	pthread_mutex_lock(&mutex);
 	list_del(&mb->list);
-	if (__isfull(mb)) 
+	if (__isfull(mb)) {
+		mb->state = UNUSABLE;
 		list_add(&mb->list, &unuse_list);
-	else
+	} else {
+		mb->state = TOUSE;
 		list_add(&mb->list, &touse_list);
+	}
 	pthread_mutex_unlock(&mutex);
 }
 
@@ -75,28 +94,24 @@ mm_blk_t *mm_reserve(void)
 			new_mb->state = INUSE;
 			pthread_mutex_unlock(&mutex);
 
+			new_mb->frst = new_mb + 1;
 			new_mb->last = ((void *) new_mb) + MEM_BLK_SIZE;
-			new_mb->tail = new_mb->last;
 			new_mb->head = new_mb->last - sizeof(struct ele_hdr);
+			new_mb->tail = new_mb->head;
 			hdr = new_mb->head;
 			hdr->free = 0;
-			hdr->size = sizeof(struct ele_hdr);
+			hdr->size = 0;
+			printf("%p reserve new\n");
 			return new_mb;
 		}
 	} 
-
 	mb = list_first_entry(&touse_list, struct mem_blk, list);
 	list_del(&mb->list);
 	list_add(&mb->list, &inuse_list);
 	mb->state = INUSE;
+	printf("%p reserve %d %d\n", mb, mb->head - mb->frst,
+	    mb->tail - mb->frst); 
 	pthread_mutex_unlock(&mutex);
-	if (mb->head - (void *) mb->frst < mb_rsvd_size) {
-		hdr = mb->head = mb->last - sizeof (struct ele_hdr);
-	} else {
-		hdr = mb->head = mb->head - sizeof (struct ele_hdr); 
-	}
-	hdr->free = 0;
-	hdr->size = sizeof (struct ele_hdr);
 	if (new_mb)
 		free(new_mb);
 	return mb;
@@ -106,18 +121,34 @@ void mm_free(mm_blk_t *mb, void *p, size_t size)
 {
 	struct ele_hdr *hdr = p + size;
 
+	printf("%p free %d %d free from %d to %d hsize %d\n", mb,
+	    mb->head - mb->frst, mb->tail - mb->frst, p - mb->frst,
+	    p + size - mb->frst, hdr->size);
+
 	hdr->free = 1;
 	pthread_mutex_lock(&mutex);
-	hdr = mb->tail - sizeof (struct ele_hdr);
-	while (hdr->free == 1) {
-		mb->tail -= hdr->size;
-		if (mb->tail - (void *) mb->frst < mb_rsvd_size)
-			mb->tail = mb->last;
-		hdr = mb->tail - sizeof(struct ele_hdr);
+	hdr = mb->tail;
+	while (hdr->free) {
+		printf("%p free loop2 %d %d from %d to %d bit:%d " 
+		    "hsize %d\n", mb, mb->head - mb->frst, 
+		    mb->tail - mb->frst,
+		    mb->tail - mb->frst + sizeof (struct ele_hdr),
+		    mb->tail - mb->frst - hdr->size, hdr->free,
+		    hdr->size);
+		hdr = mb->tail = mb->tail - hdr->size - sizeof (struct ele_hdr);
+
+		if (mb->tail < mb->head && 
+		    (mb->tail - mb->frst < mb_rsvd_size)) {
+			hdr = mb->tail = mb->last - sizeof (struct ele_hdr);
+			printf("%p free loop1 %d %d bit:%d hsize %d\n", mb,
+			    mb->head - mb->frst, mb->tail - mb->frst, hdr->free,
+			    hdr->size);
+		}
 	}
 	if (mb->state == UNUSABLE && !__isfull(mb)) {
 		list_del(&mb->list);
 		list_add(&mb->list, &touse_list);
+		mb->state = TOUSE;
 	}
 	pthread_mutex_unlock(&mutex);
 }
@@ -128,6 +159,9 @@ void *mm_alloc(mm_blk_t *mb, size_t size)
 
 	assert(mb->state == INUSE);
 	hdr->size += size;
+
+	printf("%p alloc %d %d size %d t: %d\n", mb, mb->head - mb->frst,
+	    mb->tail - mb->frst, size, hdr->size);
 	return ((void *) hdr) - hdr->size; 
 }
 
@@ -146,35 +180,60 @@ mm_system_exit()
 {
 }
 
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+static struct list_head list;
+
+struct hhh {
+	struct list_head ent;
+	size_t size;
+	mm_blk_t *mb;
+};
+
 void *test(void *arg)
 {
 	mm_blk_t *mb;
-	char *p[10], *q[100];
-	int total = 0, t[100];
-	int i, j = 0;
+	void *p;
+	int total;
+	int i;
+	struct hhh *hdr;
 
-	for (j = 0; j < 100; j++)
-		t[j] = 0;
-
-	j = 0;
 	while (1) {
+		total = 0;
 		mb = mm_reserve();
-	
 		for (i = 0; i < 10; i++) {
-			size_t len = random() % 20;
-		    	p[i] = mm_alloc(mb, len);
-			t[j] += len;
+			size_t len = 16 + random() % 20;
+		    	p = mm_alloc(mb, len);
+			total += len;
 		}
 		mm_unreserve(mb);
-		q[j++] = p[9];
-		if (j == 100) {
-			for (j=j-1; j >=  0; j--)
-				    mm_free(mb, q[j], t[j]); 
-			for (j = 0; j < 100; j++)
-				t[j] = 0;
-			j = 0;
-		}
+
+		hdr = p;
+		pthread_mutex_lock(&lock);
+		list_add(&hdr->ent, &list);
+		hdr->size = total;
+		hdr->mb = mb;
+		pthread_cond_signal(&cond);
+		pthread_mutex_unlock(&lock);
 	}
+}
+
+void *test1(void *arg)
+{
+	struct hhh *ent;
+	pthread_mutex_lock(&lock);
+	while (1) {
+		while (list_empty(&list)) {
+			pthread_cond_wait(&cond, &lock);
+		}
+		ent = list_first_entry(&list, struct hhh, ent);
+		list_del(&ent->ent);
+		pthread_mutex_unlock(&lock);
+		mm_free(ent->mb, ent, ent->size);
+		pthread_mutex_lock(&lock);
+	}
+	pthread_mutex_unlock(&lock);
 }
 
 int main()
@@ -182,10 +241,13 @@ int main()
 	pthread_t t;
 	int i;
 
-	mm_system_init(200);
-	for (i = 0; i < 10; i++)
-	    	pthread_create(&t, NULL, test, NULL);
+	INIT_LIST_HEAD(&list);
 
+	mm_system_init(400);
+
+    	pthread_create(&t, NULL, test, NULL);
+    	pthread_create(&t, NULL, test, NULL);
+    	pthread_create(&t, NULL, test1, NULL);
 	pthread_join(t, NULL);
 
 }
